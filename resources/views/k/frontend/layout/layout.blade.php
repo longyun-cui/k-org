1375628f63a9f8b184b6f7f2a03ef8deb658af1c
Lonyun Cui <longyun-cui@163.com>
2020-11-26 13:02:10 +0800

    weixin

--- resources/views/k/frontend/layout/layout.blade.php
+++ resources/views/k/frontend/layout/layout.blade.php
@@ -499,7 +499,19 @@ desired effect
                     'updateTimelineShareData',
                     'onMenuShareWeibo'
                 ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-            }) ;
+            });
+
+            wx.checkJsApi({
+                jsApiList: [
+                    'updateAppMessageShareData',
+                    'updateTimelineShareData',
+                    'onMenuShareWeibo'
+                ], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+                success: function(res) {
+                    // 以键值对的形式返回，可用的api值true，不可用为false
+                    // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+                }
+            });
 
             wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
                 wx.updateAppMessageShareData({
