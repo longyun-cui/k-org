b798b1d96bbe4cfdfca4a011b1231340099ee044
Lonyun Cui <longyun-cui@163.com>
2020-11-26 13:21:00 +0800

    weixin

--- lib/Wechat/TokenManager.php
+++ lib/Wechat/TokenManager.php
@@ -24,16 +24,16 @@ class TokenManager
         $url = 'http://'.$_SERVER["HTTP_HOST"].request()->getRequestUri();
 
 
-        if(Cache::has(self::cache_key))
-        {
-            $cache = Cache::get(self::cache_key);
-
-            $appID = $cache['app_id'];
-            $ticket = $cache['ticket'];
-            $nonce_str = $cache['nonce_str'];
-            $timestamp = $cache['timestamp'];
-        }
-        else
+//        if(Cache::has(self::cache_key))
+//        {
+//            $cache = Cache::get(self::cache_key);
+//
+//            $appID = $cache['app_id'];
+//            $ticket = $cache['ticket'];
+//            $nonce_str = $cache['nonce_str'];
+//            $timestamp = $cache['timestamp'];
+//        }
+//        else
         {
             $ticket = self::getTicket();
             $nonce_str = self::getNonceStr();
