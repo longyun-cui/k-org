c644764694c4bac25c8b8d246b51354d103fda68
Lonyun Cui <longyun-cui@163.com>
2021-01-30 22:09:25 +0800

    root

--- app/Repositories/Frontend/IndexRepository.php
+++ app/Repositories/Frontend/IndexRepository.php
@@ -74,6 +74,7 @@ class IndexRepository {
                 ->where('user_type',11)
                 ->where('user_status',1)
                 ->where('active',1)
+                ->orderByDesc('id')
                 ->paginate(20);
         }
 
