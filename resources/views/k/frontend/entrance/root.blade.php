ff2962ca318cae7d83fd6dfc81a30678c3d0fe05
Lonyun Cui <longyun-cui@163.com>
2021-02-20 21:52:22 +0800

    style

--- resources/views/k/frontend/entrance/root.blade.php
+++ resources/views/k/frontend/entrance/root.blade.php
@@ -37,23 +37,23 @@
 
         @if($page_type == 'tag')
 
-            <div class="pull-left margin-bottom-16px">
+            <div class="container-box pull-left margin-bottom-16px">
                 @if(request('type') != 'activity')
                     @include(env('TEMPLATE_DEFAULT').'frontend.component.user-list',['user_list'=>$user_list])
                 @endif
             </div>
 
-            <div class="pull-left margin-bottom-16px">
+            <div class="container-box pull-left margin-bottom-16px">
                 @include(env('TEMPLATE_DEFAULT').'frontend.component.item-list',['item_list'=>$item_list])
             </div>
 
         @else
 
-            <div class="pull-left margin-bottom-16px">
+            <div class="container-box pull-left margin-bottom-16px">
                 @include(env('TEMPLATE_DEFAULT').'frontend.component.item-list',['item_list'=>$item_list])
             </div>
 
-            <div class="pull-left margin-bottom-16px">
+            <div class="container-box pull-left margin-bottom-16px">
                 @if(request('type') != 'activity')
                     @include(env('TEMPLATE_DEFAULT').'frontend.component.user-list',['user_list'=>$user_list])
                 @endif
