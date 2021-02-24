eec25b1512c6bc58a20e3510643833b68264891a
Lonyun Cui <longyun-cui@163.com>
2021-02-25 00:03:59 +0800

    tag

--- resources/views/k/frontend/component/left-tag.blade.php
+++ resources/views/k/frontend/component/left-tag.blade.php
@@ -3,7 +3,7 @@
     <i class="fa fa-tags"></i><br>
 
     <a class="tag" href="{{ url('/tag/上海') }}">#上海</a>
-    <a class="tag" href="{{ url('/tag/上海') }}">#北京</a>
+    <a class="tag" href="{{ url('/tag/北京') }}">#北京</a>
     <a class="tag" href="{{ url('/tag/嘉兴') }}">#嘉兴</a>
     <a class="tag" href="{{ url('/tag/昆山') }}">#昆山</a>
     <a class="tag" href="{{ url('/tag/排球') }}">#排球</a>
