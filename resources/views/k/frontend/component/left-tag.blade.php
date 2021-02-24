4db00999f527fad861d7e2b2ea8abdc9d345052e
Lonyun Cui <longyun-cui@163.com>
2021-02-24 23:56:39 +0800

    tag

--- resources/views/k/frontend/component/left-tag.blade.php
+++ resources/views/k/frontend/component/left-tag.blade.php
@@ -3,8 +3,11 @@
     <i class="fa fa-tags"></i><br>
 
     <a class="tag" href="{{ url('/tag/上海') }}">#上海</a>
+    <a class="tag" href="{{ url('/tag/上海') }}">#北京</a>
     <a class="tag" href="{{ url('/tag/嘉兴') }}">#嘉兴</a>
     <a class="tag" href="{{ url('/tag/昆山') }}">#昆山</a>
     <a class="tag" href="{{ url('/tag/排球') }}">#排球</a>
+    <a class="tag" href="{{ url('/tag/老年会') }}">#老年会</a>
+    <a class="tag" href="{{ url('/tag/企业家') }}">#企业家</a>
 
 </div>
