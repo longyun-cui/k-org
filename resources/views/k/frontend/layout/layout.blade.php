30a8424fe61a3b21931de74a5bfc660020ab995b
Lonyun Cui <longyun-cui@163.com>
2021-03-03 01:19:41 +0800

    weixin.share

--- resources/views/k/frontend/layout/layout.blade.php
+++ resources/views/k/frontend/layout/layout.blade.php
@@ -248,7 +248,8 @@ desired effect
 
 
 {{--<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>--}}
-<script src="http://res.wx.qq.com/open/js/jweixin-1.6.0.js"></script>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
+{{--<script src="http://res.wx.qq.com/open/js/jweixin-1.6.0.js"></script>--}}
 <script>
 
     var wechat_config = {!! $wechat_config or '' !!};
