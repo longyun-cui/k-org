49cc9dc1cc416ab8779f5df3ec3b72a0c87f7673
Lonyun Cui <longyun-cui@163.com>
2024-04-03 16:05:37 +0800

    k

--- resources/views/k/common/root/component/component-card.blade.php
+++ resources/views/k/common/root/component/component-card.blade.php
@@ -383,7 +383,7 @@
 
 
 @if(!empty($data->ext->content) || !empty($data->ext->description))
-<div class=" item-piece item-option margin-top-4px padding-12px border-bottom-0" style="border-bottom:0;">
+<div class=" item-piece item-option margin-top-4px padding-16px border-bottom-0" style="border-bottom:0;">
     <article class="readmore-content item-piece item-option padding-0 border-bottom-0" style="border-bottom:0;">
 
         <div class="item-row margin-bottom-4px- text-center _none">
