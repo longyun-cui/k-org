ee73e36994d2883ac4fb2163da4c3d547cd7b31d
Lonyun Cui <longyun-cui@163.com>
2024-04-25 11:02:12 +0800

    www_index

--- app/Repositories/WWW/WWWIndexRepository.php
+++ app/Repositories/WWW/WWWIndexRepository.php
@@ -314,7 +314,7 @@ class WWWIndexRepository {
         $user_count_by_city = 0;
         $user_count_by_province = 0;
         $user_count_by_region = 0;
-        $q = '';
+        $q = '--';
         // 根据地理位置推荐用户
         if($ip_province_code)
         {
