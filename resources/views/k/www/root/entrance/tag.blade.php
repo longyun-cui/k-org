f6714392b626756fb4fd2c0736821a6c38cc1f8f
Lonyun Cui <longyun-cui@163.com>
2024-04-27 17:42:17 +0800

    www_index

--- resources/views/k/www/root/entrance/tag.blade.php
+++ resources/views/k/www/root/entrance/tag.blade.php
@@ -10,9 +10,9 @@
 @section('meta_keywords')朝鲜族,朝鲜族社群,朝鲜族组织,朝鲜族活动,朝鲜族社群平台,朝鲜族组织平台,朝鲜族活动平台@endsection
 
 
-@section('wx_share_title')#{{ $q or '' }}@endsection
-@section('wx_share_desc')朝鲜族社群平台@endsection
-@section('wx_share_imgUrl'){{ url('/custom/k/k-www.jpg') }}@endsection
+@section('wx_share_title')#{{ $q or '' }} - 朝鲜族社群平台@endsection
+@section('wx_share_desc')发现身边的朝鲜族社群组织活动@endsection
+@section('wx_share_imgUrl'){{ url('/custom/k/k-www-wx-share.jpg') }}@endsection
 
 
 
