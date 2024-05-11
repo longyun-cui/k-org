63192fa3ef9327fdba62fdc34a1e611625dd3c4b
Lonyun Cui <longyun-cui@163.com>
2024-05-11 23:07:17 +0800

    wx

--- config/UEditorUpload.php
+++ config/UEditorUpload.php
@@ -41,7 +41,7 @@ return [
         "imageActionName" => "uploadimage", /* 执行上传图片的action名称 */
         "imageFieldName" => "upfile", /* 提交的图片表单名称 */
         "imageMaxSize" => 2048000, /* 上传大小限制，单位B */
-        "imageAllowFiles" => [".png", ".jpg", ".jpeg", ".gif", ".bmp"], /* 上传图片格式显示 */
+        "imageAllowFiles" => [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp"], /* 上传图片格式显示 */
         "imageCompressEnable" => true, /* 是否压缩图片,默认是true */
         "imageCompressBorder" => 1200, /* 图片压缩最长边限制 */
         "imageInsertAlign" => "none", /* 插入的图片浮动方式 */
@@ -64,7 +64,7 @@ return [
         "scrawlActionName" => "uploadscrawl", /* 执行上传涂鸦的action名称 */
         "scrawlFieldName" => "upfile", /* 提交的图片表单名称 */
         "scrawlPathFormat" => "/uploads/ueditor/php/upload/image/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
-        "scrawlMaxSize" => 2048000, /* 上传大小限制，单位B */
+        "scrawlMaxSize" => 5120000, /* 上传大小限制，单位B */
         "scrawlUrlPrefix" => "", /* 图片访问路径前缀 */
         "scrawlInsertAlign" => "none",
 
@@ -121,7 +121,7 @@ return [
         "fileManagerUrlPrefix" => "", /* 文件访问路径前缀 */
         "fileManagerListSize" => 20, /* 每次列出文件数量 */
         "fileManagerAllowFiles" => [
-            ".png", ".jpg", ".jpeg", ".gif", ".bmp",
+            ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp",
             ".flv", ".swf", ".mkv", ".avi", ".rm", ".rmvb", ".mpeg", ".mpg",
             ".ogg", ".ogv", ".mov", ".wmv", ".mp4", ".webm", ".mp3", ".wav", ".mid",
             ".rar", ".zip", ".tar", ".gz", ".7z", ".bz2", ".cab", ".iso",
