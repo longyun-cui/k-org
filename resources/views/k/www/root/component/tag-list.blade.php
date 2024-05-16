2f2028eb14f91ad74cf6ee75ebb00044cdb74ad6
Lonyun Cui <longyun-cui@163.com>
2024-05-16 23:29:51 +0800

    www

--- resources/views/k/www/root/component/tag-list.blade.php
+++ resources/views/k/www/root/component/tag-list.blade.php
@@ -13,11 +13,14 @@
         <a class="tag" href="{{ url('/tag/北京') }}">#北京</a>
         <a class="tag" href="{{ url('/tag/上海') }}">#上海</a>
         <a class="tag" href="{{ url('/tag/天津') }}">#天津</a>
+        <a class="tag" href="{{ url('/tag/广州') }}">#广州</a>
+        <a class="tag" href="{{ url('/tag/杭州') }}">#杭州</a>
+        <a class="tag" href="{{ url('/tag/苏州') }}">#苏州</a>
+        <a class="tag" href="{{ url('/tag/大连') }}">#大连</a>
+        <a class="tag" href="{{ url('/tag/青岛') }}">#青岛</a>
         <a class="tag" href="{{ url('/tag/延边') }}">#延边</a>
         <a class="tag" href="{{ url('/tag/嘉兴') }}">#嘉兴</a>
-        <a class="tag" href="{{ url('/tag/苏州') }}">#苏州</a>
         <a class="tag" href="{{ url('/tag/昆山') }}">#昆山</a>
-        <a class="tag" href="{{ url('/tag/青岛') }}">#青岛</a>
         <br>
     </div>
 
@@ -73,12 +76,31 @@
         <a class="tag" href="{{ url('/tag/北京') }}">#北京</a>
         <a class="tag" href="{{ url('/tag/上海') }}">#上海</a>
         <a class="tag" href="{{ url('/tag/天津') }}">#天津</a>
+        <a class="tag" href="{{ url('/tag/广州') }}">#广州</a>
+        <a class="tag" href="{{ url('/tag/杭州') }}">#杭州</a>
+        <a class="tag" href="{{ url('/tag/苏州') }}">#苏州</a>
+        <a class="tag" href="{{ url('/tag/大连') }}">#大连</a>
+        <a class="tag" href="{{ url('/tag/青岛') }}">#青岛</a>
         <a class="tag" href="{{ url('/tag/延边') }}">#延边</a>
         <a class="tag" href="{{ url('/tag/嘉兴') }}">#嘉兴</a>
-        <a class="tag" href="{{ url('/tag/苏州') }}">#苏州</a>
         <a class="tag" href="{{ url('/tag/昆山') }}">#昆山</a>
-        <a class="tag" href="{{ url('/tag/青岛') }}">#青岛</a>
         <br>
+    </div>
+
+</div>
+
+
+<div class="box-body bg-white margin-bottom-2px">
+
+    <div class="pull-left" style="width:20px;">
+        <i class="fa fa-tags"></i>
+    </div>
+
+    <div class="pull-left" style="width:calc(100% - 24px);">
+        <a class="tag" href="{{ url('/tag/联谊会') }}">#联谊会</a>
+        <a class="tag" href="{{ url('/tag/老年会') }}">#老年会</a>
+        <a class="tag" href="{{ url('/tag/女子会') }}">#女子会</a>
+        <a class="tag" href="{{ url('/tag/企业家') }}">#企业家</a>
     </div>
 
 </div>
@@ -91,8 +113,8 @@
     </div>
 
     <div class="pull-left" style="width:calc(100% - 24px);">
-        <a class="tag" href="{{ url('/tag/老年会') }}">#老年会</a>
-        <a class="tag" href="{{ url('/tag/企业家') }}">#企业家</a>
+        <a class="tag" href="{{ url('/tag/高尔夫') }}">#高尔夫</a>
+        <a class="tag" href="{{ url('/tag/足球') }}">#足球</a>
         <a class="tag" href="{{ url('/tag/排球') }}">#排球</a>
         <a class="tag" href="{{ url('/tag/羽毛球') }}">#羽毛球</a>
     </div>
